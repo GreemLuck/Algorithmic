d7336353d1ade46fe04697caedc0232fdcafec29
Gremaud Lucien <lucien.gremaud@gmail.com>
2019-09-27 22:22:28 +0200

    started shunting yard algorithm

--- Series/src/algo18/ShuntingYardAlgorithm.java
+++ Series/src/algo18/ShuntingYardAlgorithm.java
@@ -5,6 +5,26 @@ public class ShuntingYardAlgorithm implements IShuntingYardAlgorithm
     @Override
     public Queue<Token> convertToRPN(Queue<Token> input)
     {
-        throw new UnsupportedOperationException();
+        Queue<Token> output = new Queue<>();
+        Stack<Token> operatorStack = new Stack<>();
+
+        while(!input.isEmpty()){
+            Token read = input.dequeue();
+
+            if(read.type == Token.TokenType.Numeric){
+                output.enqueue(read);
+            } else if(read.type == Token.TokenType.Function){
+                operatorStack.push(read);
+            } else if(read.type == Token.TokenType.Operator){
+
+                Token topOperator = operatorStack.peek();
+                while((topOperator.type == Token.TokenType.Function)
+                    || (topOperator.getOperatorPrecedence() >= read.getOperatorPrecedence())
+                    && (topOperator.toString() != "("))
+                {
+
+                }
+            }
+        }
     }
 }
